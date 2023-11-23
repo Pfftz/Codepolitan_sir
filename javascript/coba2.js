dad2961d0a19c061c7e2879bc67f05f0580eaefd
Pfftz <Kalixpfft+github@protonmail.com>
2023-11-23 20:20:17 +0700

    Add variables and objects to coba2.js

---
+++ javascript/coba2.js
@@ -0,0 +1,32 @@
+let name = "jawir";
+let age = 20;
+let isMarried = false;
+let address = "Jl. Malioboro, Yogjakarta";
+let hobbies = ["Gaming", "Coding", "Reading"];
+let school = {
+    highSchool: "SMA N 1",
+    university: "Universitas Gadjah Mada"
+};
+let skills = [
+    {
+        name: "Javascript",
+        level: "beginner"
+    },
+    {
+        name: "HTML",
+        level: "beginner"
+    },
+    {
+        name: "CSS",
+        level: "beginner"
+    }
+];
+let interestInCoding = true;
+
+console.log(name);
+console.log(age);
+console.log(isMarried);
+console.log(address);
+console.log(hobbies);
+console.log(school);
+console.log(skills);
