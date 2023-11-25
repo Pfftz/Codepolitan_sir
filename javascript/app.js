b7ba58ab473809ede04f1e7e22a152900a60c490
Pfftz <Kalixpfft+github@protonmail.com>
2023-11-25 21:52:47 +0700

    Add random number generator and conditional statement

--- javascript/app.js
+++ javascript/app.js
@@ -1,2 +1,8 @@
-console.log("Hello World!")
-alert("Ada Jawir!!!")
+let angka = Math.random();
+console.log(angka);
+
+if (angka >= 0.5) {
+    alert('angka lebih besar dari 0.5');
+} else {
+    alert('angka lebih kecil dari 0.5');
+}
