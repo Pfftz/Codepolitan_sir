bb5caefb46937a08122531b4d684dc0626481f06
Pfftz <Kalixpfft+github@protonmail.com>
2023-12-26 17:43:22 +0700

    Refactor code and add number guessing game

--- javascript/app.js
+++ javascript/app.js
@@ -37,19 +37,39 @@
 //     console.log('Anda hanya bisa masuk ke halaman home');
 // }
 
-const studentRow = [
-    ['Andi', 'Budi', 'Caca'],
-    ['Deni', 'Euis', 'Fafa'],
-    ['Gilang', 'Hani', 'Ivan']
-]
+// const studentRow = [
+//     ['Andi', 'Budi', 'Caca'],
+//     ['Deni', 'Euis', 'Fafa'],
+//     ['Gilang', 'Hani', 'Ivan']
+// ]
 
-for (let i = 0; i < studentRow.length; i++) {
-    const row = studentRow[i];
-    console.log(`Seat Row ${i + 1}`);
-    for (let j = 0; j < row.length; j++) {
-        console.log(`     ${row[j]}`);
+// for (let i = 0; i < studentRow.length; i++) {
+//     const row = studentRow[i];
+//     console.log(`Seat Row ${i + 1}`);
+//     for (let j = 0; j < row.length; j++) {
+//         console.log(`     ${row[j]}`);
+//     }
+// }
+
+let maximum = parseInt(prompt('Masukkan nilai maksimal: '));
+while (!maximum) {
+    maximum = parseInt(prompt('Masukkan nilai maksimal: '));
+}
+
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+console.log(targetNum);
+
+let guess = parseInt(prompt('Masukkan angka tebakan: '));
+
+while (guess !== targetNum) {
+    if(guess > targetNum) {
+        guess = parseInt(prompt('Tebakan anda terlalu besar, coba lagi: '));
+    } else {
+        guess = parseInt(prompt('Tebakan anda terlalu kecil, coba lagi: '));
     }
 }
+
+alert(`Selamat tebakan anda benar! Angka yang dicari adalah ${targetNum}`);
 
 // Path: javascript/app.js
 
