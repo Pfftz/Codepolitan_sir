93ea77776e15e92b98f52dd08f194d377b8488e2
Pfftz <Kalixpfft+github@protonmail.com>
2024-01-24 18:43:29 +0700

    Update Pokemon images and add more Pokemon to the list

--- DOM/cp-javascript-dom-main/pokemon/app.js
+++ DOM/cp-javascript-dom-main/pokemon/app.js
@@ -1,20 +1,20 @@
-// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1000.png
 
 const container = document.querySelector('#container');
 const baseImgURL =
 	'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
-for (let i = 1; i <= 500; i++) {
-	const pokeBall = document.createElement('div');
-	pokeBall.classList.add('pokemon');
-
-	const imgPokemon = document.createElement('img');
-	imgPokemon.src = `${baseImgURL}${i}.png`;
+for (let i = 1; i <= 1025; i++) {
+	const pokeDex = document.createElement('div');
+	pokeDex.classList.add('pokemon');
 
 	const label = document.createElement('span');
 	label.innerText = `#${i}`;
 
-	pokeBall.appendChild(imgPokemon);
-	pokeBall.appendChild(label);
-	container.appendChild(pokeBall);
+	const newImg = document.createElement('img');
+	newImg.src = `${baseImgURL}${i}.png`;
+
+	pokeDex.appendChild(newImg);
+	pokeDex.appendChild(label);
+	container.appendChild(pokeDex);
 }
