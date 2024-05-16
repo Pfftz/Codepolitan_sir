0e2ec3b3662247f5da5475bd4f06393c2286248d
Pfftz <Kalixpfft+github@protonmail.com>
2024-05-16 23:04:29 +0700

    chore: Add module exports for mathematical operations

---
+++ NodeJS/moduleExport/module/app.js
@@ -0,0 +1,9 @@
+const hitung = require('./rumus');
+
+console.log(hitung.penjumlahan(2, 3)); // Output: 5
+console.log(hitung.pengurangan(2, 3)); // Output: -1
+console.log(hitung.perkalian(2, 3)); // Output: 6
+console.log(hitung.pembagian(6, 3)); // Output: 2
+console.log(hitung.modulus(6, 3)); // Output: 0
+
+console.log (hitung.perkalian(2, 3)/2); // Output: 3
---
+++ NodeJS/moduleExport/module/rumus.js
@@ -0,0 +1,20 @@
+const penjumlahan = (a, b) => a + b;
+
+const pengurangan = (a, b) => a - b;
+
+const perkalian = (a, b) => a * b;
+
+const pembagian = (a, b) => a / b;
+
+const modulus = (a, b) => a % b;
+
+const matematika = {
+    penjumlahan,
+    pengurangan,
+    perkalian,
+    pembagian,
+    modulus,
+};
+
+module.exports = matematika;
+
