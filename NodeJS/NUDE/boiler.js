c1f630caed04e2e9f9131c6dae18fed6ec3f3ef6
Pfftz <Kalixpfft+github@protonmail.com>
2024-05-16 22:49:15 +0700

    chore: Create project folder and index.html file

--- NodeJS/NUDE/boiler.js
+++ NodeJS/NUDE/boiler.js
@@ -1,6 +1,17 @@
 const fs = require('fs');
+const folderName = process.argv[2] || 'Project';
 
-fs.mkdir('project', {recursive: true}, (err) => {
-    if (err) throw err;
-    console.log('project directory is created');
-});
+// fs.mkdir('project', {recursive: true}, (err) => {
+//     if (err) throw err;
+//     console.log('project directory is created asynchronously');
+// });
+
+try {
+    fs.mkdirSync(folderName);
+    fs.writeFileSync(`${folderName}/index.html`, '<h1>Hello World</h1>');
+    fs.writeFileSync(`${folderName}/app.js`, '');
+    fs.writeFileSync(`${folderName}/styles.css`, '');
+} catch (e) {
+    console.log('Something went wrong');
+    console.log(e);
+}
