61a694477698e4856079ee567b0be37915d92b1c
Pfftz <Kalixpfft+github@protonmail.com>
2024-05-21 19:18:54 +0700

    chore: Set views directory for EJS templates

--- Learn_express/express-ejs/index.js
+++ Learn_express/express-ejs/index.js
@@ -1,7 +1,9 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -9,5 +11,7 @@ app.get("/", (req, res) => {
 
 app.listen(8080, () => {
     console.log(`Server started on http://localhost:8080`);
+    console.log(`Press Ctrl+C to stop`);
+    console.log(path.join(__dirname, "views"));
 });
 
