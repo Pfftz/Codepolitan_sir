8e45f1ee3f519486c2a4b34e0f93db92e4dd359d
Pfftz <Kalixpfft+github@protonmail.com>
2024-06-21 10:41:24 +0700

    chore: Update MongoDB connection to use correct URI format

--- mongojs/index.js
+++ mongojs/index.js
@@ -1,3 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1;27017/movie_db');
+mongoose
+    .connect("mongodb://127.0.0.1:27017/movie_db")
+    .then((result) => {
+        console.log("Connected to DB");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
