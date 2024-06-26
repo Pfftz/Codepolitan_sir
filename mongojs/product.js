71295a4d63741c4f43aa8616eb0d833c2d848287
Pfftz <Kalixpfft+github@protonmail.com>
2024-06-26 19:20:54 +0700

    feat: Update product schema with additional fields and fix typo in Movie.deleteMany() method call

--- mongojs/product.js
+++ mongojs/product.js
@@ -12,23 +12,37 @@ mongoose
 const productSchema = mongoose.Schema({
     name: { type: String, required: true },
     brand: { type: String, required: true },
-    price: { type: Number, required: true, min: 0},
+    price: { type: Number, required: true, min: 0 },
     color: { type: [String], required: true },
     size: { type: [String], required: true },
-    description: { type: String, required: true, maxlength: 150},
-    condition: { type: String, enum: ["new", "used"], default: "new"},
-    stock: { type: Number, required: true, min: 0},
+    description: { type: String, required: true, maxlength: 150 },
+    condition: { type: String, enum: ["baru", "bekas"], default: "baru" },
+    stock: { type: Number, required: true, min: 0 },
     availability: {
         online: { type: Boolean, required: true },
-        offline: { type: Boolean, required: true }
+        offline: { type: Boolean, required: true },
     },
 });
 
 const Product = mongoose.model("Product", productSchema);
 
-const tshirt = new Product({ name: "ORIGOTOT", price: 500000 });
+const product = new Product({
+    name: "Kemeja Flanel",
+    brand: "Hollister",
+    price: 750000,
+    color: "biru muda",
+    size: ["S", "M", "L"],
+    description:
+        "Kemeja flanel dengan warna yang cerah, terbuat dari bahan flanel yang nyaman dan berkualitas tinggi.",
+    condition: "baru",
+    stock: 25,
+    availability: {
+        online: true,
+        offline: true,
+    },
+});
 
-// tshirt
+// product
 //     .save()
 //     .then((result) => {
 //         console.log(result);
@@ -37,10 +51,37 @@ const tshirt = new Product({ name: "ORIGOTOT", price: 500000 });
 //         console.log(err);
 //     });
 
-// Product.deleteOne({ name: "ORIGOTOT" })
+// Product.deleteMany({ name: "Kemeja Flanel" })
 //     .then((result) => {
-//         console.log("Delete operation successful", result);
+//         console.log(result);
 //     })
 //     .catch((err) => {
-//         console.log("Error during delete operation", err);
+//         console.log(err);
+//     });
+
+// Product.findOneAndUpdate(
+//     {
+//         name: "Kemeja Flanel",
+//     },
+//     {
+//         brand: "Hollister",
+//         price: 850000,
+//         color: "biru muda",
+//         size: ["S", "M", "L"],
+//         description:
+//             "Kemeja flanel dengan warna yang cerah, terbuat dari bahan flanel yang nyaman dan berkualitas tinggi.",
+//         condition: "baru",
+//         stock: 25,
+//         availability: {
+//             online: true,
+//             offline: true,
+//         },
+//     },
+//     { new: true, runValidators: true }
+// )
+//     .then((result) => {
+//         console.log(result);
+//     })
+//     .catch((err) => {
+//         console.log(err);
 //     });
