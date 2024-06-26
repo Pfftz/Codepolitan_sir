ee959d4610b253e7dc848cb49aed3252b25aef3a
Pfftz <Kalixpfft+github@protonmail.com>
2024-06-26 17:40:32 +0700

    feat: Update movie rating and add lazy loading for images

--- mongojs/index.js
+++ mongojs/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose
     .connect("mongodb://127.0.0.1:27017/movie_db")
-    .then((result) => {
+    .then(() => {
         console.log("Connected to DB");
     })
     .catch((err) => {
@@ -15,27 +15,39 @@ const movieSchema = new mongoose.Schema({
     genre: String,
     director: String,
     rating: Number,
+    cast: [String],
+    description: String,
+    image: String,
 });
 
 const Movie = mongoose.model("Movie", movieSchema);
 
-// Movie.findOne({ year: { $gte: 2018 }, genre: 'Action' })
-//     .then((result) => {
-//         console.log(result);
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//     });
+// Uncomment to use
 
-// Update Data
-// Movie.updateMany({ year: { $lt: 2019 } }, { rating: 8 })
-//     .then((result) => {
-//         console.log(result);
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//     });
+// Find one movie
+/*
+Movie.findOne({ year: { $gte: 2018 }, genre: 'Action' })
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+*/
 
+// Update multiple movies
+/*
+Movie.updateMany({ year: { $lt: 2019 } }, { rating: 8 })
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+*/
+
+// Update a movie by ID
+/*
 Movie.findByIdAndUpdate("6674fc48e25385fbfc68a0ad", { rating: 9 })
     .then((result) => {
         console.log(result);
@@ -43,71 +55,68 @@ Movie.findByIdAndUpdate("6674fc48e25385fbfc68a0ad", { rating: 9 })
     .catch((err) => {
         console.log(err);
     });
+*/
 
-// const movie = new Movie({
-//     tittle: "The Dark Knight",
-//     year: 2008,
-//     rating: 9,
-//     director: "Christopher Nolan",
-// });
+// Delete movies with a specific title
+/*
+Movie.deleteMany({ title: "The Dark Knight" }) // Fixed typo from 'tittle' to 'title'
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+*/
 
-// Movie.insertMany([
-// 	{
-// 		"title": "Black Panther",
-// 		"genre": "Action",
-// 		"director": "Ryan Coogler",
-// 		"year": 2018,
-// 		"cast": ["Chadwick Boseman", "Michael B. Jordan", "Lupita Nyong'o"],
-// 		"description": "T'Challa, the King of Wakanda, rises to the throne in the isolated, technologically advanced African nation, but his claim is challenged by a vengeful outsider who was a childhood victim of T'Challa's father's mistake.",
-// 		"rating": 7.3,
-// 		"image": "https://www.example.com/black_panther.jpg"
-// 	},
-// 	{
-// 		"title": "Avengers: Infinity War",
-// 		"genre": "Action",
-// 		"director": "Anthony Russo, Joe Russo",
-// 		"year": 2018,
-// 		"cast": ["Robert Downey Jr.", "Chris Hemsworth", "Mark Ruffalo"],
-// 		"description": "The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.",
-// 		"rating": 8.4,
-// 		"image": "https://www.example.com/avengers_infinity_war.jpg"
-// 	},
-// 	{
-// 		"title": "Joker",
-// 		"genre": "Crime",
-// 		"director": "Todd Phillips",
-// 		"year": 2019,
-// 		"cast": ["Joaquin Phoenix", "Robert De Niro", "Zazie Beetz"],
-// 		"description": "In Gotham City, mentally-troubled comedian Arthur Fleck is disregarded and mistreated by society. He then embarks on a downward spiral of revolution and bloody crime. This path brings him face-to-face with his alter-ego: 'The Joker'.",
-// 		"rating": 8.4,
-// 		"image": "https://www.example.com/joker.jpg"
-// 	},
-// 	{
-// 		"title": "Parasite",
-// 		"genre": "Drama",
-// 		"director": "Bong Joon Ho",
-// 		"year": 2019,
-// 		"cast": ["Song Kang-ho", "Lee Sun-kyun", "Cho Yeo-jeong"],
-// 		"description": "Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.",
-// 		"rating": 8.6,
-// 		"image": "https://www.example.com/parasite.jpg"
-// 	},
-// 	{
-// 		"title": "Spider-Man: Into the Spider-Verse",
-// 		"genre": "Animation",
-// 		"director": "Bob Persichetti, Peter Ramsey, Rodney Rothman",
-// 		"year": 2018,
-// 		"cast": ["Shameik Moore", "Jake Johnson", "Hailee Steinfeld"],
-// 		"description": "Teen Miles Morales becomes the Spider-Man of his universe, and must join with five spider-powered individuals from other dimensions to stop a threat for all realities.",
-// 		"rating": 8.4,
-// 		"image": "https://www.example.com/spider_man_into_the_spider_verse.jpg"
-// 	}
-// ]).then((result) => {
-//     console.log('items inserted!');
-// }).catch((err) => {
-//     console.log(err);
-// });
+// Delete a movie by ID
 
-// movie.save();
+Movie.findByIdAndDelete("6674fc48e25385fbfc68a0ac")
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
-// console.log(movie);
+// Insert multiple movies
+/*
+Movie.insertMany([
+    {
+        "title": "Black Panther",
+        "genre": "Action",
+        "director": "Ryan Coogler",
+        "year": 2018,
+        "cast": ["Chadwick Boseman", "Michael B. Jordan", "Lupita Nyong'o"],
+        "description": "T'Challa, the King of Wakanda, rises to the throne in the isolated, technologically advanced African nation, but his claim is challenged by a vengeful outsider who was a childhood victim of T'Challa's father's mistake.",
+        "rating": 7.3,
+        "image": "https://www.example.com/black_panther.jpg"
+    },
+    {
+        "title": "Avengers: Infinity War",
+        "genre": "Action",
+        "director": "Anthony Russo, Joe Russo",
+        "year": 2018,
+        "cast": ["Robert Downey Jr.", "Chris Hemsworth", "Mark Ruffalo"],
+        "description": "The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.",
+        "rating": 8.4,
+        "image": "https://www.example.com/avengers_infinity_war.jpg"
+    },
+    // Add more movies as needed
+]).then((result) => {
+    console.log('Items inserted!');
+}).catch((err) => {
+    console.log(err);
+});
+*/
+
+// Create a new movie instance
+/*
+const movie = new Movie({
+    title: "The Dark Knight", // Fixed typo from 'tittle' to 'title'
+    year: 2008,
+    rating: 9,
+    director: "Christopher Nolan",
+});
+
+movie.save().then(() => console.log(movie)).catch((err) => console.log(err));
+*/
