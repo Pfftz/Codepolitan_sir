c573e0d6387c3a0d6e956357eb2cfe4c6dbab16c
Pfftz <Kalixpfft+github@protonmail.com>
2024-07-02 19:29:51 +0700

    feat: Add route and view for editing individual product details

--- project_mongoose/cobaBuat/index.js
+++ project_mongoose/cobaBuat/index.js
@@ -18,6 +18,7 @@ mongoose
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send("Hello World");
@@ -32,7 +33,19 @@ app.get("/products/create", (req, res) => {
     res.render("products/create");
 });
 
+app.post("/products", async (req, res) => {
+    const product = new Product(req.body);
+    await product.save();
+    res.redirect(`/products/${product._id}`);
+});
+
 app.get("/products/:id", async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render("products/show", { product });
+});
+
+app.get("/products/:id/edit", async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
     res.render("products/show", { product });
