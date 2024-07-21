4a3abc954445beddc5f508c489b93d00bd4ac789
Pfftz <Kalixpfft+github@protonmail.com>
2024-07-22 01:51:19 +0700

    feat: Add validation middleware for place creation and editing

--- YELPCLONE/app.js
+++ YELPCLONE/app.js
@@ -2,6 +2,7 @@
 const ejsMate = require("ejs-mate");
 const express = require("express");
 const ErrorHandler = require("./utils/ErrorHandler");
+const Joi = require("joi");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const wrapAsync = require("./utils/wrapAsync");
@@ -10,6 +11,9 @@ const app = express();
 
 //models
 const Place = require("./models/place");
+
+//schemas
+const { placeSchema } = require("./schemas/place");
 
 //connect to mongodb
 mongoose
@@ -29,6 +33,16 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+const validatePlace = (req, res, next) => {
+    const { error } = placeSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map((el) => el.message).join(",");
+        return next(new ErrorHandler(error, 400));
+    } else {
+        next();
+    }
+};
+
 app.get("/", (req, res) => {
     res.render("home");
 });
@@ -47,6 +61,7 @@ app.get("/places/create", (req, res) => {
 
 app.post(
     "/places",
+    validatePlace,
     wrapAsync(async (req, res, next) => {
         const place = new Place(req.body.place);
         await place.save();
@@ -72,6 +87,7 @@ app.get(
 
 app.put(
     "/places/:id",
+    validatePlace,
     wrapAsync(async (req, res) => {
         const { id } = req.params;
         const place = await Place.findByIdAndUpdate(id, { ...req.body.place });
