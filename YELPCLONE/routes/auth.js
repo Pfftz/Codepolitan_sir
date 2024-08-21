96a7ca1322499f0761f91893104a9d5db5b144b1
Pfftz <Kalixpfft+github@protonmail.com>
2024-08-21 21:03:26 +0700

    feat: Refactor authentication routes and controllers

--- YELPCLONE/routes/auth.js
+++ YELPCLONE/routes/auth.js
@@ -1,37 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const AuthController = require("../controllers/auth");
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
 
-router.get("/register", (req, res) => {
-    res.render("auth/register");
-});
+router.get("/register", AuthController.registerForm);
 
 router.post(
     "/register",
-    wrapAsync(async (req, res, next) => {
-        try {
-            const { email, username, password } = req.body;
-            const user = new User({ email, username });
-            const registerUser = await User.register(user, password);
-            req.login(registerUser, (err) => {
-                if (err) {
-                    return next(err);
-                }
-                req.flash("success_msg", "Welcome to BestPoints");
-                res.redirect("/places");
-            });
-        } catch (err) {
-            req.flash("error_msg", err.message);
-            res.redirect("/register");
-        }
-    })
+    wrapAsync(AuthController.register)
 );
 
-router.get("/login", (req, res) => {
-    res.render("auth/login");
-});
+router.get("/login", AuthController.loginForm);
 
 router.post(
     "/login",
@@ -42,20 +23,9 @@ router.post(
         },
         failureRedirect: "/login",
     }),
-    (req, res) => {
-        req.flash("success_msg", "Welcome back!");
-        res.redirect("/places");
-    }
+    AuthController.login
 );
 
-router.post("/logout", (req, res) => {
-    req.logout(function (err) {
-        if (err) {
-            return next(err);
-        }
-        req.flash("success_msg", "Logged out successfully");
-        res.redirect("/login");
-    });
-});
+router.post("/logout", AuthController.logout);
 
 module.exports = router;
